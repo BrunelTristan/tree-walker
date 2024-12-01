9418746aa939e66bd3c17035241aeef1ef600222
TBR <TBR@tbr.com>
2024-12-01 21:24:37 +0100

    implement neighbor finder (don't manage multiple presence)

--- internal/treeHelpers/neighborFinder.go
+++ internal/treeHelpers/neighborFinder.go
@@ -12,5 +12,19 @@ func NewNeighborFinder() *NeighborFinder {
 }
 
 func (nf NeighborFinder) GetNeighbors(sourceTree *tree.Tree, currentNode *tree.Node) []*tree.Node {
-	return nil
+	if sourceTree == nil || currentNode == nil || len(sourceTree.Links) == 0 {
+		return nil
+	}
+
+	neighbors := []*tree.Node{}
+
+	for _, link := range sourceTree.Links {
+		if link.Nodes[0] == currentNode {
+			neighbors = append(neighbors, link.Nodes[1])
+		} else if link.Nodes[1] == currentNode {
+			neighbors = append(neighbors, link.Nodes[0])
+		}
+	}
+
+	return neighbors
 }
--- internal/treeHelpers/neighborFinder_test.go
+++ internal/treeHelpers/neighborFinder_test.go
@@ -57,3 +57,95 @@ func TestGetNeighborsWithNoUsefulNode(t *testing.T) {
 
 	assert.Empty(t, neighbors, "Should not find any neighbor")
 }
+
+func TestGetNeighborsWithJustOneLink(t *testing.T) {
+	baseTree := tree.Tree{
+		Nodes: []tree.Node{
+			tree.Node{ID: 0},
+			tree.Node{ID: 1},
+			tree.Node{ID: 2},
+			tree.Node{ID: 3},
+			tree.Node{ID: 4},
+			tree.Node{ID: 8},
+		},
+	}
+	baseTree.Links = []tree.Link{
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[0], &baseTree.Nodes[5]}},
+	}
+
+	someNode := &baseTree.Nodes[0]
+	otherNode := &baseTree.Nodes[5]
+
+	finder := NewNeighborFinder()
+
+	neighbors := finder.GetNeighbors(&baseTree, someNode)
+
+	assert.NotEmpty(t, neighbors, "Should find a neighbor")
+	assert.Len(t, neighbors, 1, "Should have one neighbor")
+	if 1 == len(neighbors) {
+		assert.Equal(t, otherNode, neighbors[0], "Should have the neighbor")
+	}
+}
+
+func TestGetNeighborsWithJustOneLinkBackwards(t *testing.T) {
+	baseTree := tree.Tree{
+		Nodes: []tree.Node{
+			tree.Node{ID: 0},
+			tree.Node{ID: 1},
+			tree.Node{ID: 2},
+			tree.Node{ID: 3},
+			tree.Node{ID: 4},
+			tree.Node{ID: 8},
+		},
+	}
+	baseTree.Links = []tree.Link{
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[5], &baseTree.Nodes[0]}},
+	}
+
+	someNode := &baseTree.Nodes[0]
+	otherNode := &baseTree.Nodes[5]
+
+	finder := NewNeighborFinder()
+
+	neighbors := finder.GetNeighbors(&baseTree, someNode)
+
+	assert.NotEmpty(t, neighbors, "Should find a neighbor")
+	assert.Len(t, neighbors, 1, "Should have one neighbor")
+	if 1 == len(neighbors) {
+		assert.Equal(t, otherNode, neighbors[0], "Should have the neighbor")
+	}
+}
+
+func TestGetNeighborsWithMultipleLinks(t *testing.T) {
+	baseTree := tree.Tree{
+		Nodes: []tree.Node{
+			tree.Node{ID: 0},
+			tree.Node{ID: 1},
+			tree.Node{ID: 2},
+			tree.Node{ID: 3},
+			tree.Node{ID: 4},
+			tree.Node{ID: 8},
+		},
+	}
+	baseTree.Links = []tree.Link{
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[5], &baseTree.Nodes[0]}},
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[5], &baseTree.Nodes[4]}},
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[3], &baseTree.Nodes[2]}},
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[0], &baseTree.Nodes[1]}},
+		tree.Link{Nodes: [2]*tree.Node{&baseTree.Nodes[0], &baseTree.Nodes[3]}},
+	}
+
+	someNode := &baseTree.Nodes[0]
+
+	finder := NewNeighborFinder()
+
+	neighbors := finder.GetNeighbors(&baseTree, someNode)
+
+	assert.NotEmpty(t, neighbors, "Should find neighbors")
+	assert.Len(t, neighbors, 3, "Should have three neighbors")
+	if 3 == len(neighbors) {
+		assert.Contains(t, neighbors, &baseTree.Nodes[1], "Should contains node 1")
+		assert.Contains(t, neighbors, &baseTree.Nodes[3], "Should contains node 3")
+		assert.Contains(t, neighbors, &baseTree.Nodes[5], "Should contains node 5")
+	}
+}
