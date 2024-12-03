74ce23510e34dc381fa83bd27356dff7ea830bdd
TBR <TBR@tbr.com>
2024-12-03 15:42:20 +0100

    read flags from command line

--- cmd/tree-walker/tree-walker.go
+++ cmd/tree-walker/tree-walker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"tree-walker/internal/composition"
 	"tree-walker/model/configuration"
@@ -10,7 +11,18 @@ func main() {
 	fmt.Println("WALK")
 
 	conf := &configuration.LaunchingConfiguration{}
+	flag.UintVar(&conf.NodeCount, "nodes", 30, "nodes count in the tree")
+
+	flag.Parse()
 
 	root := composition.NewCompositionRoot(conf)
 	root.Build()
+
+	walker := root.ComposeWalker()
+	tree := root.ComposeTree()
+
+	path := walker.Walk(tree, &tree.Nodes[0], &tree.Nodes[len(tree.Nodes)-1])
+
+	fmt.Printf("Path from Node %d to %d is ", tree.Nodes[0].ID, tree.Nodes[len(tree.Nodes)-1].ID)
+	fmt.Println(path)
 }
